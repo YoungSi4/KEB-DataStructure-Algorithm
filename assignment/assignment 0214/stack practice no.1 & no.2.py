06fd97cf97919e229ede9443d9c5e1906c9cbb7c
YoungSi4 <dksdudgns01@gmail.com>
2024-02-15 23:03:26 +0900

    assignment stack

--- assignment/assignment 0214/stack practice no.1 & no.2.py
+++ assignment/assignment 0214/stack practice no.1 & no.2.py
@@ -1,13 +1,73 @@
+## 스택 응용문제 01
+## 헨젤과 그레텔의 집으로 돌아가기
+
+# import random
+#
+# ## 전역변수
+# stones = ['주황', '초록', '파랑', '보라', '노랑', '빨강', '노랑']
+# stack = []
+#
+# ## 메인 함수
+# if __name__ == "__main__":
+#     random.shuffle(stones)
+#
+#     print("과자집에 가는 길: ", end='')
+#     for stone in stones:
+#         stack.append(stone)
+#         print(f"{stone}-->", end='')
+#     print('과자집')
+#
+#     print("우리집에 오는 길: ", end='')
+#     for i in range(len(stones)):
+#         print(f'{stones.pop()}-->', end='')
+#     print('우리집')
 
 
+"""
+_______________________________________________________________
+"""
 
 
+## 응용예제 2
+## 파일 내용을 완전히 거꾸로 출력하기
+
+# stack = []
+#
+# if __name__ == "__main__" :
+#
+# 	with open("진달래꽃.txt", 'r', encoding='UTF8') as rfp :
+# 		line_array = rfp.readlines()
+#
+# 	print("---------- 원본 ----------")
+# 	for line in line_array :
+# 		stack.append(line)
+# 		print(line, end = ' ')
+# 	print()
+#
+# 	print("------ 거꾸로 처리된 결과 ------")
+# 	while True :
+# 		line = stack.pop()
+# 		if line == None :
+# 			break
+#
+# 		mini_stack = [None for _ in range(len(line))]
+# 		mini_top = -1
+#
+# 		for charact in line :
+# 			mini_top += 1
+# 			mini_stack[mini_top] = charact
+#
+# 		while True :
+# 			if mini_top == -1 :
+# 				break
+# 			charact = mini_stack[mini_top]
+# 			mini_top -= 1
+# 			print(charact, end =' ')
 
 
-
-
-
-
+"""
+-------------------------------------------------------------
+"""
 
 
 """
